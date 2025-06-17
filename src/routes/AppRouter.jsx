7930dcb465310b1d66ce4e24d652bfe8ca8883c7
Sandeep Verma <sandeepverma9525@gmail.com>
2025-06-17 11:22:59 +0530

    remove animation video

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,53 +1,6 @@
-// import React from 'react';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-// import Home from '../pages/Home';
-// import About from '../pages/About';
-// import Product from '../pages/Product';
-// import ContactUs from '../pages/ContactUs';
-// import Enqiry from '../pages/Enqiry';
-// import Entertainment from '../pages/Entertainment';
-// import TopSpeed from '../pages/TopSpeed';
-// import Nav from '../components/Nav';
-// import Footer from '../components/Footer';
-
-// const AppRouter = () => {
-//   return (
-//     <BrowserRouter>
-//       <div style={{ maxWidth: '1600px', margin: '0 auto', overflowX: 'hidden' }}>
-
-//         <Nav />
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/About" element={<About />} />
-//           <Route path="/Product" element={<Product />} />
-//           <Route path="/ContactUs" element={<ContactUs />} />
-//           <Route path="/Enqiry" element={<Enqiry />} />
-//           <Route path="/Entertainment" element={<Entertainment />} />
-//           <Route path="/TopSpeed" element={<TopSpeed />} />
-//         </Routes>
-//         <Footer />
-//       </div>
-//     </BrowserRouter>
-//   );
-// };
-
-// export default AppRouter;
-
-
-
-
-
-
-
-
-
-
-
 import React from 'react';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import IntroVideo from '../pages/IntroVideo';
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Product from '../pages/Product';
@@ -58,32 +11,79 @@ import TopSpeed from '../pages/TopSpeed';
 import Nav from '../components/Nav';
 import Footer from '../components/Footer';
 
-const AppContent = () => {
-  const location = useLocation();
-  const hideLayout = location.pathname === '/'; // Hide on video page
-
+const AppRouter = () => {
   return (
-    <div style={{ maxWidth: '1600px', margin: '0 auto', overflowX: 'hidden' }}>
-      {!hideLayout && <Nav />}
-      <Routes>
-        <Route path="/" element={<IntroVideo />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Product" element={<Product />} />
-        <Route path="/ContactUs" element={<ContactUs />} />
-        <Route path="/Enqiry" element={<Enqiry />} />
-        <Route path="/Entertainment" element={<Entertainment />} />
-        <Route path="/TopSpeed" element={<TopSpeed />} />
-      </Routes>
-      {!hideLayout && <Footer />}
-    </div>
+    <BrowserRouter>
+      <div style={{ maxWidth: '1600px', margin: '0 auto', overflowX: 'hidden' }}>
+
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Product" element={<Product />} />
+          <Route path="/ContactUs" element={<ContactUs />} />
+          <Route path="/Enqiry" element={<Enqiry />} />
+          <Route path="/Entertainment" element={<Entertainment />} />
+          <Route path="/TopSpeed" element={<TopSpeed />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 };
 
-const AppRouter = () => (
-  <BrowserRouter>
-    <AppContent />
-  </BrowserRouter>
-);
-
 export default AppRouter;
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+
+// import IntroVideo from '../pages/IntroVideo';
+// import Home from '../pages/Home';
+// import About from '../pages/About';
+// import Product from '../pages/Product';
+// import ContactUs from '../pages/ContactUs';
+// import Enqiry from '../pages/Enqiry';
+// import Entertainment from '../pages/Entertainment';
+// import TopSpeed from '../pages/TopSpeed';
+// import Nav from '../components/Nav';
+// import Footer from '../components/Footer';
+
+// const AppContent = () => {
+//   const location = useLocation();
+//   const hideLayout = location.pathname === '/'; // Hide on video page
+
+//   return (
+//     <div style={{ maxWidth: '1600px', margin: '0 auto', overflowX: 'hidden' }}>
+//       {!hideLayout && <Nav />}
+//       <Routes>
+//         <Route path="/" element={<IntroVideo />} />
+//         <Route path="/home" element={<Home />} />
+//         <Route path="/About" element={<About />} />
+//         <Route path="/Product" element={<Product />} />
+//         <Route path="/ContactUs" element={<ContactUs />} />
+//         <Route path="/Enqiry" element={<Enqiry />} />
+//         <Route path="/Entertainment" element={<Entertainment />} />
+//         <Route path="/TopSpeed" element={<TopSpeed />} />
+//       </Routes>
+//       {!hideLayout && <Footer />}
+//     </div>
+//   );
+// };
+
+// const AppRouter = () => (
+//   <BrowserRouter>
+//     <AppContent />
+//   </BrowserRouter>
+// );
+
+// export default AppRouter;
